fbadb37a824ec3c31b0d06d7bab9b71cddacca6b
roniboukheir1 <roni.boukheir@lau.edu>
2025-03-22 21:41:42 +0200

    added user CRUD operations file

---
+++ Backend/app/CRUD/users.py
@@ -0,0 +1,37 @@
+from sqlalchemy import Session
+from app.models.user import User
+
+def get_user(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(User).filter(User.username == username).first()
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(User).offset(skip).limit(limit).all()
+
+def create_user(db: Session, username: str, email: str, hashed_password: str):
+    user = User(username=username, email=email, hashed_password=hashed_password)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+def update_user(db: Session, user_id: int, username: str, email: str, hashed_password: str):
+    user = db.query(User).filter(User.id == user_id).first()
+    user.username = username
+    user.email = email
+    user.hashed_password = hashed_password
+    db.commit()
+    db.refresh(user)
+    return user
+
+def delete_user(db: Session, user_id: int):
+    user = db.query(User).filter(User.id == user_id).first()
+    db.delete(user)
+    db.commit()
+    return user
+
